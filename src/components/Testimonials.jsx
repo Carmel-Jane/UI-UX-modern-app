6116e2dbd4c0fa12003d45c0f57bba3adc18db5a
carmeljane <carmelbarwick@gmail.com>
2024-05-02 16:44:30 +0100

    started testimonial section,what people saying about us written so far

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import styles from "../style";
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
-export default Testimonials
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2 className={styles.heading2}>
+        What People are <br className="sm:block hidden" /> saying about us
+      </h2>
+    </div>
+  </section>
+);
+
+export default Testimonials;
