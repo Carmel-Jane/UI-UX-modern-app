45f23b1112b99a354e55fee17e156618e793956c
carmeljane <carmelbarwick@gmail.com>
2024-05-02 11:52:20 +0100

    hero set up

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,12 @@ const App = () => (
         Navbar
       </div>
     </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
 
+    </div>
   </div>
 )
 
